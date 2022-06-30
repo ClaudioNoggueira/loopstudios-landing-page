9e9de9480e97f3090f37a1201fc59b0cbcb0e35b
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-06-30 10:46:25 -0300

    425px media query

--- src/sections/Creations.tsx
+++ src/sections/Creations.tsx
@@ -1,14 +1,23 @@
 import Button from "../components/Button";
 import ImageCard from "../components/ImageCard";
 
-import curiosity from "../images/desktop/creations/image-curiosity.jpg";
-import deepEarth from "../images/desktop/creations/image-deep-earth.jpg";
-import fisheye from "../images/desktop/creations/image-fisheye.jpg";
-import fromAbove from "../images/desktop/creations/image-from-above.jpg";
-import grid from "../images/desktop/creations/image-grid.jpg";
-import nightArcade from "../images/desktop/creations/image-night-arcade.jpg";
-import pocketBorealis from "../images/desktop/creations/image-pocket-borealis.jpg";
-import soccerTeam from "../images/desktop/creations/image-soccer-team.jpg";
+import desktopCuriosity from "../images/desktop/creations/image-curiosity.jpg";
+import desktopDeepEarth from "../images/desktop/creations/image-deep-earth.jpg";
+import desktopFisheye from "../images/desktop/creations/image-fisheye.jpg";
+import desktopFromAbove from "../images/desktop/creations/image-from-above.jpg";
+import desktopGrid from "../images/desktop/creations/image-grid.jpg";
+import desktopNightArcade from "../images/desktop/creations/image-night-arcade.jpg";
+import desktopPocketBorealis from "../images/desktop/creations/image-pocket-borealis.jpg";
+import desktopSoccerTeam from "../images/desktop/creations/image-soccer-team.jpg";
+
+import mobileCuriosity from "../images/mobile/creations/image-curiosity.jpg";
+import mobileDeepEarth from "../images/mobile/creations/image-deep-earth.jpg";
+import mobileFisheye from "../images/mobile/creations/image-fisheye.jpg";
+import mobileFromAbove from "../images/mobile/creations/image-from-above.jpg";
+import mobileGrid from "../images/mobile/creations/image-grid.jpg";
+import mobileNightArcade from "../images/mobile/creations/image-night-arcade.jpg";
+import mobilePocketBorealis from "../images/mobile/creations/image-pocket-borealis.jpg";
+import mobileSoccerTeam from "../images/mobile/creations/image-soccer-team.jpg";
 
 import "../sections/styles/Creations.css";
 
@@ -20,15 +29,32 @@ export default function Creations() {
         <Button textContent="See All" className="btn--full" />
       </div>
 
-      <div className="grid grid--4-cols">
-        <ImageCard imageURL={deepEarth} textContent="Deep Earth" />
-        <ImageCard imageURL={nightArcade} textContent="Night Arcade" />
-        <ImageCard imageURL={soccerTeam} textContent="Soccer Team VR" />
-        <ImageCard imageURL={grid} textContent="The Grid" />
-        <ImageCard imageURL={fromAbove} textContent="From up above vr" />
-        <ImageCard imageURL={pocketBorealis} textContent="Pocket Borealis" />
-        <ImageCard imageURL={curiosity} textContent="The curiosity" />
-        <ImageCard imageURL={fisheye} textContent="Make it Fisheye" />
+      <div className="grid grid--4-cols desktop-image">
+        <ImageCard imageURL={desktopDeepEarth} textContent="Deep Earth" />
+        <ImageCard imageURL={desktopNightArcade} textContent="Night Arcade" />
+        <ImageCard imageURL={desktopSoccerTeam} textContent="Soccer Team VR" />
+        <ImageCard imageURL={desktopGrid} textContent="The Grid" />
+        <ImageCard imageURL={desktopFromAbove} textContent="From up above vr" />
+        <ImageCard
+          imageURL={desktopPocketBorealis}
+          textContent="Pocket Borealis"
+        />
+        <ImageCard imageURL={desktopCuriosity} textContent="The curiosity" />
+        <ImageCard imageURL={desktopFisheye} textContent="Make it Fisheye" />
+      </div>
+
+      <div className="grid grid--4-cols mobile-image">
+        <ImageCard imageURL={mobileDeepEarth} textContent="Deep Earth" />
+        <ImageCard imageURL={mobileNightArcade} textContent="Night Arcade" />
+        <ImageCard imageURL={mobileSoccerTeam} textContent="Soccer Team VR" />
+        <ImageCard imageURL={mobileGrid} textContent="The Grid" />
+        <ImageCard imageURL={mobileFromAbove} textContent="From up above vr" />
+        <ImageCard
+          imageURL={mobilePocketBorealis}
+          textContent="Pocket Borealis"
+        />
+        <ImageCard imageURL={mobileCuriosity} textContent="The curiosity" />
+        <ImageCard imageURL={mobileFisheye} textContent="Make it Fisheye" />
       </div>
     </section>
   );
